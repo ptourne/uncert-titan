7b1b7ae566df77285fac6219591b2b5a82811c8c
patriciotourne <patriciotourne@estudiantesorganizados.ar>
2023-10-04 22:15:13 -0300

    - Start Menu - Pause Menu - Running

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,9 +1,36 @@
 extends CharacterBody2D
 
-const ACCELERATION = 1000
-const MAX_SPEED = 100
-const FRICTION = 1000
+const WALKING_ACCELERATION = 600
+const RUNNIN_ACCELERATION = 1000
+const MAX_WALKING_SPEED = 80
+const MAX_RUNNIN_SPEED = 150
+const MAX_SPEED = 150
+const WALKING_FRICTION = 800
+const RUNNIN_FRICTION = 1200
 
+var running = false
+
+func is_running():
+	return running
+
+func max_speed():
+	if self.is_running():
+		return MAX_RUNNIN_SPEED
+	else:
+		return MAX_WALKING_SPEED
+
+func acceleration():
+	if self.is_running():
+		return RUNNIN_ACCELERATION
+	else:
+		return WALKING_ACCELERATION
+		
+func friction():
+	if self.is_running():
+		return RUNNIN_FRICTION
+	else:
+		return WALKING_FRICTION
+		
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	
@@ -12,8 +39,15 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+		velocity = velocity.move_toward(input_vector * self.max_speed(), self.acceleration() * delta)
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		velocity = velocity.move_toward(Vector2.ZERO, self.friction() * delta)
 		
 	move_and_slide()
+
+
+func _input(event):
+	if event.is_action_pressed("ui_run"):
+		running = true
+	if event.is_action_released("ui_run"):
+		running = false
