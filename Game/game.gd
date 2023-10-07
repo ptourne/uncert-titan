d9530633c106530e69124eb7f2d1120a165afa93
Patricio Tourne Passarino <ptourne@fi.uba.ar>
2023-10-07 12:19:13 -0300

    Die of oxigen

--- Game/game.gd
+++ Game/game.gd
@@ -5,6 +5,7 @@ class_name GameManager
 @export var player : Player
 
 signal toggle_game_paused(is_paused: bool)
+signal die(message)
 
 var game_paused: bool = false:
 	get:
@@ -18,7 +19,8 @@ func _ready():
 	player.connect("die", _die)
 
 func _die(message):
-	print(message)
+	game_paused = true
+	die.emit(message)
 
 func _input(event: InputEvent):
 	if (event.is_action_pressed("ui_cancel")):
